2d667e8a57fed135af3f783dd36010a72ddd5005
Fabio Robledo Batoni de Godoy <fabiobatoni98@gmail.com>
2025-09-24 11:23:03 -0300

    create user route and middleware for check session_id

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import fastify from 'fastify'
-// import { userRoutes } from './routes/user.js'
 import cookie from '@fastify/cookie'
+import { usersRoutes } from './routes/users.js'
 
 export const app = fastify()
 
 app.register(cookie)
-app.register(userRoutes, {
+app.register(usersRoutes, {
   prefix: 'user',
 })
