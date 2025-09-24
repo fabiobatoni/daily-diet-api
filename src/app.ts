7d73f5de6d825cc971dee692c03ee88e12449503
Fabio Robledo Batoni de Godoy <fabiobatoni98@gmail.com>
2025-09-24 11:37:06 -0300

    adjusts app meals routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 import { usersRoutes } from './routes/users.js'
+import { mealsRoutes } from './routes/meals.js'
 
 export const app = fastify()
 
 app.register(cookie)
 app.register(usersRoutes, {
   prefix: 'users',
+})
+
+app.register(mealsRoutes, {
+  prefix: 'meals',
 })
